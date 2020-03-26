d018366c53613d23a77197f8b96b6d3ada9059b5
Andrea Franco <andrea07021981@gmail.com>
2020-03-25 21:14:33 -0400

    Added transitions and some corrections

--- app/src/main/java/com/example/savethefood/MainActivity.kt
+++ app/src/main/java/com/example/savethefood/MainActivity.kt
@@ -11,6 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //TODO add the coroutineworker to update db info
     }
 
     override fun onSupportNavigateUp(): Boolean {
