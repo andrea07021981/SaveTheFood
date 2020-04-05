645185e7270f97492687e9ed4ba26697978a0e3e
Andrea Franco <andrea07021981@gmail.com>
2020-04-05 18:10:15 -0400

    Started recipe details UI

--- app/src/main/java/com/example/savethefood/util/RecipeBindingUtils.kt
+++ app/src/main/java/com/example/savethefood/util/RecipeBindingUtils.kt
@@ -2,6 +2,7 @@ package com.example.savethefood.util
 
 import android.view.View
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatRatingBar
 import androidx.core.net.toUri
