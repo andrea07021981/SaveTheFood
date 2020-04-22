8fdb63bb2306dfb52ccda8341516ffc7a9af69ca
Andrea Franco <andrea07021981@gmail.com>
2020-04-22 11:33:59 -0400

    Images for readme

--- app/src/main/java/com/example/savethefood/network/service/ApiKey.kt
+++ app/src/main/java/com/example/savethefood/network/service/ApiKey.kt
@@ -1,6 +1,5 @@
 package com.example.savethefood.network.service
 
 object ApiKey {
-    //TODO HIDE API_KEY for release
-    const val API_KEY = "f3c973b4895e42fdb658a319644cc30b"
+    const val API_KEY = "YOUR-API-KEY"
 }
