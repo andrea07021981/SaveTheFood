776230767df995ff669759fe746f0f214a4a3933
Andrea Franco <andrea07021981@gmail.com>
2020-05-17 22:35:57 -0400

    COrrections navigation

--- app/src/main/java/com/example/savethefood/addfood/BarcodeReaderFragment.kt
+++ app/src/main/java/com/example/savethefood/addfood/BarcodeReaderFragment.kt
@@ -2,15 +2,14 @@ package com.example.savethefood.addfood
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.example.savethefood.EventObserver
+import com.example.savethefood.R
 import com.example.savethefood.data.Result
 import com.example.savethefood.data.source.repository.FoodDataRepository
 import com.example.savethefood.databinding.FragmentBarcodereaderBinding
@@ -36,6 +35,7 @@ class BarcodeReaderFragment : Fragment() {
         dataBinding.lifecycleOwner = this
         dataBinding.barcodeReaderViewModel = barcodeReaderViewModel
         dataBinding.food = barcodeReaderViewModel.food.value
+        setHasOptionsMenu(true)
         return dataBinding.root
     }
 
@@ -91,5 +91,17 @@ class BarcodeReaderFragment : Fragment() {
         override fun startActivityForResult(intent: Intent, code: Int) {
             fragment.startActivityForResult(intent, code)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.menu_barcode, menu)
+        // check if the activity resolves
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return super.onOptionsItemSelected(item)
     }
 }
