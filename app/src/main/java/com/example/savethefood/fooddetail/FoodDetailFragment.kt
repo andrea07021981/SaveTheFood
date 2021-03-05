bd2a0867561b95be2bd1a372f701d2c007758311
Andrea Franco <andrea07021981@gmail.com>
2021-03-05 17:28:27 +0100

    Started new detail layout

--- app/src/main/java/com/example/savethefood/fooddetail/FoodDetailFragment.kt
+++ app/src/main/java/com/example/savethefood/fooddetail/FoodDetailFragment.kt
@@ -1,6 +1,7 @@
 package com.example.savethefood.fooddetail
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.viewModels
 import androidx.navigation.ActivityNavigator
 import androidx.navigation.fragment.navArgs
@@ -8,13 +9,13 @@ import androidx.transition.TransitionInflater
 import com.example.savethefood.BaseFragment
 import com.example.savethefood.R
 import com.example.savethefood.databinding.FragmentFoodDetailBinding
-import com.example.savethefood.home.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
+
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
-class FoodDetailFragment : BaseFragment<FoodDetailViewModel,FragmentFoodDetailBinding>() {
+class FoodDetailFragment : BaseFragment<FoodDetailViewModel, FragmentFoodDetailBinding>() {
 
     private val args: FoodDetailFragmentArgs by navArgs()
 
@@ -35,10 +36,14 @@ class FoodDetailFragment : BaseFragment<FoodDetailViewModel,FragmentFoodDetailBi
         with(dataBinding) {
             foodDetailViewModel = viewModel
         }
+
+        //TODO change expiring color based on how many days (gree, yellow and red)
     }
 
     override fun activateObservers() {
-
+        viewModel.food.observe(viewLifecycleOwner, {
+            (activity as AppCompatActivity?)!!.supportActionBar!!.title = it.title
+        })
     }
 
     override fun onDestroy() {
