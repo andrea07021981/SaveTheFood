8a4d785d9ccb6d055aaee254b7749454e2a4ded9
Andrea Franco <andrea07021981@gmail.com>
2021-05-24 18:51:30 +0200

    Started with bad and food for groceries

--- app/src/main/java/com/example/savethefood/shopping/BagFragment.kt
+++ app/src/main/java/com/example/savethefood/shopping/BagFragment.kt
@@ -1,15 +1,17 @@
 package com.example.savethefood.shopping
 
 import android.util.Log
+import android.view.View
 import androidx.fragment.app.viewModels
 import com.example.savethefood.BaseFragment
+import com.example.savethefood.FragmentCallback
 import com.example.savethefood.R
 import com.example.savethefood.data.Result
 import com.example.savethefood.databinding.FragmentBagBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class BagFragment : BaseFragment<BagViewModel, FragmentBagBinding>() {
+class BagFragment : BaseFragment<BagViewModel, FragmentBagBinding>(), FragmentCallback {
 
     override val viewModel: BagViewModel by viewModels()
 
@@ -27,5 +29,9 @@ class BagFragment : BaseFragment<BagViewModel, FragmentBagBinding>() {
 
     override fun activateObservers() {
 
+    }
+
+    override fun onAddClicked(view: View) {
+        TODO("Not yet implemented, call the add food in bag")
     }
 }
