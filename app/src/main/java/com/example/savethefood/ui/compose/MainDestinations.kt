c762ee3c9ee4ccb7ef7e27809c89aba741cef06c
Andrea Franco <andrea07021981@gmail.com>
2021-10-15 17:16:35 -0400

    Added animation from splash Added navgraph

---
+++ app/src/main/java/com/example/savethefood/ui/compose/MainDestinations.kt
@@ -0,0 +1,90 @@
+package com.example.savethefood.ui.compose
+
+object MainDestinations {
+    const val HOME_ROUTE = "home"
+    const val FOOD_ROUTE = "food"
+    const val RECIPE_ROUTE = "recipe"
+    const val COOKING_ROUTE = "cooking"
+}
+
+// TODO use same as Rally screen but inside body add everything. Need to add navcontroller in constructor line 56
+
+/*
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.compose.rally
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AttachMoney
+import androidx.compose.material.icons.filled.MoneyOff
+import androidx.compose.material.icons.filled.PieChart
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.example.compose.rally.data.UserData
+import com.example.compose.rally.ui.accounts.AccountsBody
+import com.example.compose.rally.ui.bills.BillsBody
+import com.example.compose.rally.ui.overview.OverviewBody
+
+/**
+ * Screen metadata for Rally.
+ */
+enum class RallyScreen(
+    val icon: ImageVector,
+    val body: @Composable ((String) -> Unit) -> Unit
+) {
+    Overview(
+        icon = Icons.Filled.PieChart,
+        body = { OverviewBody() }
+    ),
+    Accounts(
+        icon = Icons.Filled.AttachMoney,
+        body = {
+            com.example.compose.rally.ui.accounts.AccountsBody(
+                accounts = com.example.compose.rally.data.UserData.accounts,
+                onAccountClick = { name ->
+                    navigateToSingleAccount(
+                        navController = navController,
+                        accountName = name
+                    )
+                }
+            )
+        }
+    ),
+    Bills(
+        icon = Icons.Filled.MoneyOff,
+        body = { BillsBody(UserData.bills) }
+    );
+
+    // Removed after navhost implementation
+/*    @Composable
+    fun content(onScreenChange: (String) -> Unit) {
+        body(onScreenChange)
+    }*/
+
+    companion object {
+        fun fromRoute(route: String?): RallyScreen =
+            when (route?.substringBefore("/")) {
+                Accounts.name -> Accounts
+                Bills.name -> Bills
+                Overview.name -> Overview
+                null -> Overview
+                else -> throw IllegalArgumentException("Route $route is not recognized.")
+            }
+    }
+}
+
+ */
