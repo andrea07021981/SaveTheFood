4624f0027dadfa1db0575f8f5e43b380d37ea33f
Andrea Franco <andrea07021981@gmail.com>
2021-12-12 17:00:21 -0500

    Completed refactor auth

--- app/src/main/java/com/example/savethefood/ui/compose/auth/SignUpScreen.kt
+++ app/src/main/java/com/example/savethefood/ui/compose/auth/SignUpScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.savethefood.R
 import com.example.savethefood.shared.data.domain.UserDomain
 import com.example.savethefood.shared.utils.LoginAuthenticationStates
 import com.example.savethefood.shared.utils.LoginStateValue
